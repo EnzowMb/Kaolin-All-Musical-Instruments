2f58b112927be7110692dba27fbe6697452a49f5
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2023-08-02 23:30:50 -0300

    chore: implemented dependecy - prisma

--- BackEnd/src/index.ts
+++ BackEnd/src/index.ts
@@ -4,7 +4,7 @@ import { Request, Response } from 'express';
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('hello mark, king of zap zap');
+  res.send('hello mark');
 });
 
 app.listen(3000, () => console.log('listening on port 3000!'));
