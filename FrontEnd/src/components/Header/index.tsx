823a7673cc5dbf03b0f9f428c6b60fe9874f51b6
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2023-11-21 16:22:38 -0300

    refactor: header

--- FrontEnd/src/components/Header/index.tsx
+++ FrontEnd/src/components/Header/index.tsx
@@ -1,26 +1,36 @@
-import { LogoHeader } from "../LogoHeader";
+import logo from "./assets/Logo.png";
 import { OpcaoHeader } from "../OptionsHeader";
 import { Link } from "react-router-dom";
 import "./header.css";
 import { UserHeader } from "../UserHeader";
+import styled from "styled-components";
 
-// const HeaderContainer = styled.header`
-//   background-color: #fcdc7b;
-//   display: flex;
-//   justify-content: center;
-// `;
+const HeaderContainer = styled.header`
+  background-color: var(--Khaki);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2em 4em;
+`;
+
+const LogoHeader = styled.img`
+  margin-right: 1em;
+  width: 50%;
+  border-radius: 2em;
+  border: 2px solid black;
+`;
 
 const Header = () => {
   return (
-    <header className="header">
+    <HeaderContainer>
       <Link to="/">
-        <LogoHeader />
+        <LogoHeader src={logo} alt="logo Kaolin" />
       </Link>
       <OpcaoHeader />
       <Link to="/dashboard">
         <UserHeader />
       </Link>
-    </header>
+    </HeaderContainer>
   );
 };
 
