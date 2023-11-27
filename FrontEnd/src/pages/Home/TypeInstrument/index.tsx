1e1ded1c02ba0e9eb54b45eea1382736ddc02d8e
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2023-11-27 17:18:20 -0300

    style: implement style home

--- FrontEnd/src/pages/Home/TypeInstrument/index.tsx
+++ FrontEnd/src/pages/Home/TypeInstrument/index.tsx
@@ -17,35 +17,58 @@ const Section = styled.div`
   padding: 16px 8px;
   box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.15);
   border-radius: 8px;
+  cursor: pointer;
 `;
 
 const Text = styled.p`
   line-height: 19px;
-  color: blue;
+  color: var(--SaddleBrown);
+  padding: 0.5em 0.5em;
+  border-radius: 1em;
+  background-color: white;
+  &:hover {
+    color: black;
+    border: 0.5px solid black;
+  }
 `;
 
 const Img = styled.img`
   width: 50%;
 `;
 
+const Link = styled.a`
+  text-decoration: none;
+`;
+
 export const TypeInstrument = () => {
   return (
     <Container>
       <Section>
-        <Img src={string} alt="String" />
-        <Text>Cordas</Text>
+        <Link href="/cordas">
+          <Img src={string} alt="String" />
+          <Text>Cordas</Text>
+        </Link>
       </Section>
+
       <Section>
-        <Img src={woodwind} alt="Woodwind" />
-        <Text>Madeiras</Text>
+        <Link href="/madeiras">
+          <Img src={woodwind} alt="Woodwind" />
+          <Text>Madeiras</Text>
+        </Link>
       </Section>
+
       <Section>
-        <Img src={brass} alt="Brass" />
-        <Text>Metais</Text>
+        <Link href="/metais">
+          <Img src={brass} alt="Brass" />
+          <Text>Metais</Text>
+        </Link>
       </Section>
+
       <Section>
-        <Img src={percussion} alt="Percussion" />
-        <Text>Percussão</Text>
+        <Link href="/percussao">
+          <Img src={percussion} alt="Percussion" />
+          <Text>Percussão</Text>
+        </Link>
       </Section>
     </Container>
   );
