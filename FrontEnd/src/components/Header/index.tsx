f7039df5c5f9ca6d0802a16eb5a78c83a5c26a1b
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2023-11-27 11:21:12 -0300

    style: remove userlogo and options folder

--- FrontEnd/src/components/Header/index.tsx
+++ FrontEnd/src/components/Header/index.tsx
@@ -1,8 +1,8 @@
 import logo from "./assets/Logo.png";
-import { OpcaoHeader } from "../OptionsHeader";
+import userLogo from "../../Img/User-Logo.png";
 import { Link } from "react-router-dom";
-import { UserHeader } from "../UserHeader";
 import styled from "styled-components";
+import { authenticStore } from "../../stores/authentic.store";
 
 const textOptions = ["CORDAS", "MADEIRAS", "METAIS", "PERCUSSAO"];
 
@@ -45,7 +45,39 @@ const Option = styled.li`
   min-width: 120px;
 `;
 
+const StyledLink = styled.a`
+  font-weight: 400;
+  color: blue;
+`;
+
+const StyledButton = styled.a`
+  background-color: var(--MistyRose);
+  border-radius: 8px;
+  padding: 12px 16px;
+  color: black;
+  text-decoration: none;
+  transition: background-color 0.3s;
+  margin: 0.3em;
+  &:hover {
+    background-color: var(--Thistle);
+  }
+`;
+
+const UserLogo = styled.img`
+  width: 50%;
+  transition: transform 0.3s;
+  &:hover {
+    transform: scale(1.05);
+    /* content: url("../../Img/User-Logo-gif.gif"); */
+  }
+`;
+
 const Header = () => {
+  const handleLogout = () => {
+    authenticStore.logout();
+  };
+  const { user } = authenticStore;
+
   return (
     <HeaderContainer>
       <Container>
@@ -61,7 +93,24 @@ const Header = () => {
             </Link>
           ))}
         </Options>
-        <UserHeader />
+        <div>
+          <Link to="/dashboard">
+            <UserLogo src={userLogo} alt="USER-LOGO" />
+          </Link>
+          {authenticStore.isAuthentic ? (
+            <div>
+              <p>Bem vindo! {user.email}</p>
+              <StyledLink href="/" onClick={handleLogout}>
+                Sair
+              </StyledLink>
+            </div>
+          ) : (
+            <div>
+              <StyledButton href="/login">Entrar</StyledButton>
+              <StyledButton href="/register-user">Cadastre-se</StyledButton>
+            </div>
+          )}
+        </div>
       </Container>
     </HeaderContainer>
   );
